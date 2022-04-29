0a4fc68efdd96c7e5a9ca8ff636a1e799f023bef
Tarik1700 <71398993+Tarik1700@users.noreply.github.com>
2022-04-29 03:01:02 +0200

    update 25
    started implementing time to ready

--- src/Components/Basket.js
+++ src/Components/Basket.js
@@ -31,15 +31,16 @@ const [isOpen2, setIsOpen2] = useState(false)
 
   
   const closeBasket = () => {
-   
+   if(test.length > 0){
     setTimeout(function() {  
-      document.getElementById("comments").style.display = "none";
-    
+     onClose1();
     setIsOpen2(true);
   
     }, 500)
     document.getElementById("basket").style.transform = "translate(-50%,432px)";
-    
+  } else{
+    alert("Basket is empty")
+  }
   }
 
   const closeBasketArrow = () => {
@@ -58,19 +59,21 @@ const [isOpen2, setIsOpen2] = useState(false)
 
 
 
-  if(!open1) return null
+  if(!open1) return <TimeToReady></TimeToReady> 
 
   return (
       <>
       <div id="backdrop" className=" bg-[rgba(255,255,255,0)]  bg-clip-padding backdrop-filter backdrop-blur-[8px] bg-opacity-60  fixed top-0 left-0 right-0 bottom-0 z-[999]"/>
    
-
+<div className="hidden">
+<TimeToReady isOpen2={isOpen2} test={test} itemsPrice={itemsPrice} > </TimeToReady>
+</div>
 
 <div id="basket" className=" transition-all fixed top-[50vh] left-52 w-[22rem] h-auto font-[poppins]  transform -translate-x-[50%] 
      bg-[#F5F5F5]  z-[999] rounded-xl translate-y-[27rem] duration-700  ">
 
       <div className=" bg-white m-2 top-2 h-auto pb-3  rounded-xl shadow-[0px_10px_15px_-2px_rgba(0,0,0,0.5)]  relative">
-        <button onClick={closeBasketArrow} className="z-[1000] flex pl-2 pt-2 absolute focus:outline-none [-webkit-tap-highlight-color:rgba(0,0,0,0);]" > <svg width="29" height="24" viewBox="0 0 29 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <button id="btn3" onClick={closeBasketArrow} className="z-[1000] flex pl-2 pt-2 absolute focus:outline-none [-webkit-tap-highlight-color:rgba(0,0,0,0);]" > <svg width="29" height="24" viewBox="0 0 29 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M0.939337 10.9393C0.35355 11.5251 0.35355 12.4749 0.939337 13.0607L10.4853 22.6066C11.0711 23.1924 12.0208 23.1924 12.6066 22.6066C13.1924 22.0208 13.1924 21.0711 12.6066 20.4853L4.12132 12L12.6066 3.51472C13.1924 2.92893 13.1924 1.97919 12.6066 1.3934C12.0208 0.807611 11.0711 0.807611 10.4853 1.3934L0.939337 10.9393ZM28.1725 10.5L2 10.5V13.5L28.1725 13.5V10.5Z" fill="black" fillOpacity="0.44"/>
 </svg> </button>
      
@@ -89,9 +92,9 @@ const [isOpen2, setIsOpen2] = useState(false)
       {item.qty} x {item.price.toFixed(2)} KM
        </div>
        
-     <button className=" text-right text-5xl  justify-end mx-auto 
+     <button id="btn1" className=" text-right text-5xl  justify-end mx-auto 
       my-auto text-[#5DBB63]" onClick={()=>passingtest2(item)}>+</button> 
-     <button className=" text-right self text-5xl   justify-end mx-auto 
+     <button id="btn2" className=" text-right self text-5xl   justify-end mx-auto 
       my-auto text-red-700 " onClick={()=>passingtest2Remove(item)} >-</button> 
  </div>
     
@@ -113,7 +116,7 @@ const [isOpen2, setIsOpen2] = useState(false)
         </div>
 
         <div className="pt-1">
-        <TimeToReady isOpen2={isOpen2}> </TimeToReady>
+        
 <div id="comments" className="">
         <div className="text-sm mt-6 relative font-[poppins]">
             <h1 className="font-extrabold text-2xl font-poppins pl-3 pb-2">Comments</h1>
--- src/Components/TimeToReady.js
+++ src/Components/TimeToReady.js
@@ -3,7 +3,7 @@ import {useState} from 'react'
 
 const TimeToReady = ( props) => {
 
-    const {open1, onClose1, isOpen2 } = props;
+    const {open1, onClose1, isOpen2, test, itemsPrice } = props;
 
 
   function myLoop3() {        
@@ -31,17 +31,66 @@ const TimeToReady = ( props) => {
     
   }
 
+  const closeBasketArrow = () => {
+    setTimeout(function() {  
+    onClose1();
+
+    }, 500)
+    document.getElementById("basket").style.transform = "translate(-50%,432px)";
+    
+  }
+  
+
 
 
   if (!isOpen2) return null
 
   return (
       
-   
 
-<div id="basket12" className=" transition-all relative  left-[50%] w-[22rem] h-auto font-[poppins]  transform -translate-x-[50%] 
-     bg-[#F5F5F5]  z-[999] rounded-xl  duration-700  ">
 
+<div id="basket" className=" transition-all fixed top-[50vh] left-52 w-[22rem] h-auto font-[poppins]  transform -translate-x-[50%] 
+     bg-[#F5F5F5]  z-[999] rounded-xl translate-y-[27rem] duration-700  ">
+
+      <div className=" bg-white m-2 top-2 h-auto pb-3  rounded-xl shadow-[0px_10px_15px_-2px_rgba(0,0,0,0.5)]  relative">
+        <button id="btn3" onClick={closeBasketArrow} className="z-[1000] flex pl-2 pt-2 absolute focus:outline-none [-webkit-tap-highlight-color:rgba(0,0,0,0);]" > <svg width="29" height="24" viewBox="0 0 29 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M0.939337 10.9393C0.35355 11.5251 0.35355 12.4749 0.939337 13.0607L10.4853 22.6066C11.0711 23.1924 12.0208 23.1924 12.6066 22.6066C13.1924 22.0208 13.1924 21.0711 12.6066 20.4853L4.12132 12L12.6066 3.51472C13.1924 2.92893 13.1924 1.97919 12.6066 1.3934C12.0208 0.807611 11.0711 0.807611 10.4853 1.3934L0.939337 10.9393ZM28.1725 10.5L2 10.5V13.5L28.1725 13.5V10.5Z" fill="black" fillOpacity="0.44"/>
+</svg> </button>
+     
+<div> {console.log(props.passingtest2) }</div>
+     
+
+        <h1 className=" relative text-2xl pl-4 pt-10 font-bold">Bill:</h1>
+        <hr className="mx-4 bg-black "></hr>
+
+        <div className="">
+        {test.length === 0 && <div className="text-xl text-center">Basket is empty</div>}
+{ test.map((item) => ( 
+    <div key = {item.id} className="col-2  text-center text-xl grid grid-cols-9 ">
+      <h1 className="mx-auto my-auto text-base col-span-3 pl-2">{item.name}</h1>
+      <div className="col-span-4 text-lg my-auto ">
+      {item.qty} x {item.price.toFixed(2)} KM
+       </div>
+       
+     
+ </div>
+    
+
+       
+    )) }
+    {test.length !== 0 && (
+      <>
+      <hr className="mx-4 bg-black"></hr>
+      <div className="grid grid-cols-2 justify-items-start mt-4">
+        <div className=" my-auto font-extrabold pl-4 text-2xl">Total:</div>
+        <div className=" my-auto  text-2xl font-extrabold justify-self-end pr-6">{itemsPrice.toFixed(2)} KM</div>
+      </div>
+      </>
+    )}
+</div>  
+
+
+        </div>
         <div className="text-sm mt-6 relative font-[poppins]">
             <h1 className="font-extrabold text-2xl font-poppins pl-3 ">Order status:</h1>
             <div className="h-24 z-[999] bg-white relative mb-3 rounded-xl m-2 shadow-[0px_10px_15px_-2px_rgba(0,0,0,0.5)]  ">
